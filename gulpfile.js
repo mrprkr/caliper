e5f0f5dcdbfb7b83d95ec60a2bfe16a202ceee37
Michael Parker <parker_michael@me.com>
2015-07-26 16:57:38 +1000

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+	jshint = require('gulp-jshint'),
+	jshintReporter = require('jshint-stylish'),
+	bower = require('gulp-bower'),
+	watch = require('gulp-watch');
+
+/*
+ * Create variables for our project paths so we can change in one place
+ */
+var paths = {
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+};
+
+
+// gulp lint
+gulp.task('lint', function(){
+	gulp.src(paths.src)
+		.pipe(jshint())
+		.pipe(jshint.reporter(jshintReporter));
+
+});
+
+// gulp watcher for lint
+gulp.task('watch:lint', function () {
+	gulp.src(paths.src)
+		.pipe(watch())
+		.pipe(jshint())
+		.pipe(jshint.reporter(jshintReporter));
+});
+
+gulp.task('bower', function(){
+	return bower()
+		.pipe(gulp.dest('public/js/lib'))
+})
